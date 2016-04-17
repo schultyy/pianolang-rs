39d8f4148712050b3f640fa320138a1bc28ae3e0
Jan Schulte <hello@unexpected-co.de>
2016-04-17 17:15:43 +0200

    tie together parser and traverser in main module

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,19 @@ mod parser;
 mod traverser;
 
 fn main() {
-    println!("Hello, world!");
+    let program = "(+ 1 5)";
+
+    let ast = match parser::parse_program(program) {
+        Ok(ast) => ast,
+        Err(err) => {
+            println!("ERR: {}", err);
+            return;
+        }
+    };
+
+    let mut traverser = traverser::Traverser::new();
+    traverser.traverse(ast);
+    let instructions = traverser.instructions();
+
+    println!("{:?}", instructions);
 }
